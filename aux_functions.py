8fa51c01956013d0ccd9d70a6ea165c0339e9d1c
Sergio García Heredia <sergio.garcia.heredia.2000@gmail.com>
2024-07-10 12:45:55 +0200

    Create aux_functions.py

---
+++ aux_functions.py
@@ -0,0 +1,8 @@
+from sklearn.metrics import pairwise_distances
+
+def get_sigma(X, q=0.5):
+    distances = pairwise_distances(X)
+    distances = distances.flatten()
+    sigma = np.quantile(distances[distances > 0], q)
+
+    return sigma
