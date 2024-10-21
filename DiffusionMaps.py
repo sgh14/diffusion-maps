Decouple calculations and plots. Fix Diffusion Maps fit method. Improve D calculation

DiffusionMaps.py
@@ -2,6 +2,7 @@ import numpy as np
 from numba import njit
 from sklearn.base import BaseEstimator, TransformerMixin
 from kernels import rbf_kernel, laplacian_kernel
+from sklearn.metrics import pairwise_distances
 
 
 class DiffusionMaps(TransformerMixin, BaseEstimator):
@@ -101,29 +102,39 @@ class DiffusionMaps(TransformerMixin, BaseEstimator):
 
     @staticmethod
     @njit
-    def diffusion_distances(P, d):
+    def diffusion_distances(P, pi):
         """
         Compute diffusion distances.
 
         Args:
             P (np.array): Diffusion probability matrix.
-            d (np.array): Degree vector.
+            pi (np.array): Stationary distribution.
 
         Returns:
             np.array: Matrix of diffusion distances.
         """
-        D = np.zeros(P.shape)
-        # Compute stationary distribution
-        pi = d / np.sum(d)
-        for i in range(P.shape[0]):
-            for j in range(i+1, P.shape[1]):
-                # Compute diffusion distance between points i and j
-                D_ij = np.sqrt(np.sum(((P[i, :] - P[j, :])**2) / pi))
-                # Store the distance (matrix is symmetric)
-                D[i, j] = D_ij
-                D[j, i] = D_ij
+        diff_dist = lambda P_i, P_j: np.sqrt(np.sum(((P_i - P_j)**2) / pi))
+        D = pairwise_distances(P, metric=diff_dist)
+        # D = np.zeros(P.shape)
+        # for i in range(P.shape[0]):
+        #     for j in range(i+1, P.shape[1]):
+        #         # Compute diffusion distance between points i and j
+        #         D_ij = (P[i, i]**2 - P[j, i]**2)/pi[i] + (P[j, j]**2 - P[i, j]**2)/pi[j]
+        #         # Store the distance (matrix is symmetric)
+        #         D[i, j] = D_ij
+        #         D[j, i] = D_ij
 
         return D
+    
+
+    @staticmethod
+    @njit
+    def _get_A(K):
+        d_i = np.sum(K, axis=1)
+        d_j = np.sum(K, axis=0)
+        A = K/np.outer(np.sqrt(d_i), np.sqrt(d_j))
+
+        return A
 
 
     @staticmethod
@@ -170,9 +181,9 @@ class DiffusionMaps(TransformerMixin, BaseEstimator):
             Corresponding eigenvectors.
         """
         # Compute the eigenvalues and right eigenvectors
-        eigenvalues, eigenvectors = np.linalg.eig(A)
-        eigenvalues = np.real(eigenvalues) # np.real_if_close(eigenvalues, tol=1e10)
-        eigenvectors = np.real(eigenvectors) # np.real_if_close(eigenvectors, tol=1e10)
+        eigenvalues, eigenvectors = np.linalg.eigh(A)
+        # eigenvalues = np.real(eigenvalues)
+        # eigenvectors = np.real(eigenvectors)
         # Find the order of the eigenvalues (decreasing order)
         order = np.argsort(eigenvalues)[::-1]
         # Sort eigenvalues and eigenvectors
@@ -200,16 +211,24 @@ class DiffusionMaps(TransformerMixin, BaseEstimator):
         # Compute the kernel
         self.K = self.get_kernel(self.X, self.X, self.sigma, self.kernel, self.alpha)
         # Compute the matrix P
-        self.P = self._get_P(self.K)
+        # self.P = self._get_P(self.K)
+        # Compute the matrix A
+        self.A = self._get_A(self.K)
         # Get the eigenvalues and eigenvectors of P
-        self.lambdas, self.psis = self._spectral_decomposition(self.P)
+        self.lambdas, self.phis = self._spectral_decomposition(self.A)
         # Fix eigenvectors orientation
-        self.psis = DiffusionMaps._fix_vector_orientation(self.psis)
+        self.phis = DiffusionMaps._fix_vector_orientation(self.phis)
         # Reduce dimension
         lambdas_red = self.lambdas[1:self.n_components + 1]
-        psis_red = self.psis[:, 1:self.n_components + 1]
+        phis_red = self.phis[:, 1:self.n_components + 1]
+        # Compute degree vector
+        d = np.sum(self.K, axis=0)
+        # Compute the stationary distribution
+        self.pi = d / np.sum(d)
+        # Compute P right eigenvectors
+        psis_red = phis_red / self.pi[:, np.newaxis]
         # Compute the new coordinates
-        self.Psi_steps = psis_red * (lambdas_red ** self.steps)
+        self.Psi_steps = psis_red * (lambdas_red[np.newaxis, :] ** self.steps)
 
         return self
 

experiments/helix/experiment.py
@@ -1,18 +1,25 @@
-import numpy as np
 import time
+import numpy as np
 import tensorflow as tf
+import os
+import random
 from os import path
+import h5py
 
 from DiffusionMaps import DiffusionMaps
 from aux_functions import get_sigma
-from experiments.helix.plot_results import my_colormap1D, plot_original, plot_projection, plot_history
-from experiments.helix.load_data import normalize, get_datasets
+from experiments.helix.load_data import normalize, my_colormap1D, get_datasets
 from experiments.utils import build_decoder
-from experiments.helix.metrics import compute_metrics
     
-
+# ENSURE REPRODUCIBILITY
 seed = 123
+os.environ['PYTHONHASHSEED'] = str(seed)
+random.seed(seed)
+np.random.seed(seed)
 tf.random.set_seed(seed)
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # force the use of CPU
+
+
 root = 'experiments/helix/results'
 titles = [
     'Few samples without noise',
@@ -20,7 +27,7 @@ titles = [
     'Few samples with noise',
     'Many samples with noise'
 ]
-datasets_train, datasets_test = get_datasets(npoints=2000, test_size=0.5, seed=123, noise=0.1)
+datasets_train, datasets_test = get_datasets(npoints=2000, test_size=0.5, seed=seed, noise=0.1)
 
 q_vals = [2e-2, 2e-2, 5e-3, 5e-3]
 steps_vals = [100, 100, 100, 100]
@@ -28,11 +35,12 @@ alpha_vals = [1, 1, 1, 1]
 kernel = 'rbf'
 
 for i in range(len(titles)):
+    title = titles[i]
     q, steps, alpha = q_vals[i], steps_vals[i], alpha_vals[i]
-    experiment = f'percentile_{q}-steps_{steps}-alpha_{alpha}'
+    experiment = f'quantile_{q}-steps_{steps}-alpha_{alpha}'
+    output_dir = path.join(root, title, experiment)
     X_train, y_train = datasets_train[i]
     X_test, y_test = datasets_test[i]
-    title = titles[i]
     sigma = get_sigma(X_train, q)
 
     print(experiment, '-', title)
@@ -49,37 +57,35 @@ for i in range(len(titles)):
     X_train_rec = decoder(X_train_red)
     X_test_rec = decoder(X_test_red)
 
-    plot_original(X_train, y_train, path.join(root, title), 'train_orig')
-    plot_original(X_test, y_test, path.join(root, title), 'test_orig')
-    plot_projection(X_train_red, y_train, path.join(root, title, experiment), 'train_red')
-    plot_original(X_train_rec, y_train, path.join(root, title, experiment), 'train_rec')
-    plot_projection(X_test_red, y_test, path.join(root, title, experiment), 'test_red')
-    plot_original(X_test_rec, y_test, path.join(root, title, experiment), 'test_rec')
-    plot_history(history, path.join(root, 'histories', title, experiment), log_scale=True)
-
-    time_in_sample = tac - tic
-    time_out_of_sample = toc - tac
-    compute_metrics(
-        X_train,
-        X_train_red,
-        X_train_rec,
-        X_test,
-        X_test_red,
-        X_test_rec,
-        time_in_sample,
-        time_out_of_sample,
-        title,
-        output_dir=path.join(root, title, experiment)
-    )
-
-    P = DM.P
+    P = DM._get_P(DM.K)
     if steps > 1:
         P = np.linalg.matrix_power(P, steps)
 
-    D = DM.diffusion_distances(P, np.sum(DM.K, axis=1))
+    D = DM.diffusion_distances(P, DM.pi)
     point = np.argmin(X_train[:, 2])
     P_color = np.array([my_colormap1D(x) for x in normalize(P[:, point])])
     D_color = np.array([my_colormap1D(x) for x in normalize(D[:, point])])
 
-    plot_original(X_train, P_color, path.join(root, title, experiment), 'train_P')
-    plot_original(X_train, D_color, path.join(root, title, experiment), 'train_D')
+    decoder.save(path.join(output_dir, 'decoder.h5'))
+    with h5py.File(path.join(output_dir, 'history.h5'), 'w') as file:
+        for key, value in history.history.items():
+            file.create_dataset(key, data=value)
+
+    with h5py.File(path.join(output_dir, 'results.h5'), "w") as file:
+        file.create_dataset("X_train", data=X_train, compression='gzip')
+        file.create_dataset("X_train_red", data=X_train_red, compression='gzip')
+        file.create_dataset("X_train_rec", data=X_train_rec, compression='gzip')
+        file.create_dataset("y_train", data=y_train, compression='gzip')
+        file.create_dataset("X_test", data=X_test, compression='gzip')
+        file.create_dataset("X_test_red", data=X_test_red, compression='gzip')
+        file.create_dataset("X_test_rec", data=X_test_rec, compression='gzip')
+        file.create_dataset("y_test", data=y_test, compression='gzip')
+        
+        file.create_dataset("P_color", data=P_color, compression='gzip')
+        file.create_dataset("D_color", data=D_color, compression='gzip')
+
+    time_in_sample = tac - tic
+    time_out_of_sample = toc - tac
+    times = np.array([time_in_sample, time_out_of_sample])
+    np.savetxt(path.join(output_dir, 'times.txt'), times)
+    

experiments/helix/plot_results.py
@@ -1,31 +1,35 @@
 import os
+from os import path
+import h5py
 import numpy as np
 from matplotlib import pyplot as plt
 
 plt.style.use('experiments/science.mplstyle')
 
 
-def my_colormap1D(x):
-    c1=(0.75, 0, 0.75)
-    c2=(0, 0.75, 0.75)
-    # Calculate the RGB values based on interpolation
-    color = np.array(c1) * (1 - x) + np.array(c2) * x
+def set_equal_ranges(ax):
+    # Get the current axis limits
+    x_min, x_max = ax.get_xlim()
+    y_min, y_max = ax.get_ylim()
+    
+    # Calculate the ranges of the x and y axes
+    x_range = x_max - x_min
+    y_range = y_max - y_min
 
-    return color
+    # Find the maximum range between x and y
+    max_range = max(x_range, y_range)
 
+    # Set new limits with the same range for both axes
+    x_center = (x_max + x_min) / 2
+    y_center = (y_max + y_min) / 2
 
-def my_colormap2D(x, y):
-    # Define colors in RGB
-    bottom_left = (0.5, 0, 0.5) # dark magenta
-    bottom_right = (0, 0.5, 0.5) # dark cyan
-    top_left = (1, 0, 1) # magenta
-    top_right = (0, 1, 1) # cyan
+    ax.set_xlim(x_center - max_range / 2, x_center + max_range / 2)
+    ax.set_ylim(y_center - max_range / 2, y_center + max_range / 2)
 
-    # Calculate the RGB values based on interpolation
-    top_color = np.array(top_left) * (1 - x) + np.array(top_right) * x
-    bottom_color = np.array(bottom_left) * (1 - x) + np.array(bottom_right) * x
+    # Set equal aspect ratio
+    ax.set_aspect('equal', adjustable='box')
 
-    return top_color * (1 - y) + bottom_color * y
+    return ax
 
 
 def plot_original(X, y, output_dir, filename):
@@ -33,18 +37,19 @@ def plot_original(X, y, output_dir, filename):
 
     fig, ax = plt.subplots(figsize=(3, 3), subplot_kw={"projection": "3d"})
     ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, alpha=1)
-    ax.set_xlabel('$x$')
+    # ax.set_xlabel('$x_1$')
     ax.set_xlim([-1, 1])
-    ax.set_ylabel('$y$')
+    # ax.set_ylabel('$x_2$')
     ax.set_ylim([-1, 1])
-    ax.set_zlabel('$z$')
+    # ax.set_zlabel('$x_3$')
     ax.set_zlim([-1, 1])
     ax.view_init(15, -72)
-    ax.dist = 12
+    # ax.dist = 12
+    ax.grid(False)
     fig.tight_layout()
 
     for format in ('.pdf', '.png', '.svg'):
-        fig.savefig(os.path.join(output_dir, filename + format))
+        fig.savefig(path.join(output_dir, filename + format))
 
     plt.close(fig)
 
@@ -54,24 +59,28 @@ def plot_projection(X, y, output_dir, filename):
 
     fig, ax = plt.subplots(figsize=(3, 3), constrained_layout=True)
     ax.scatter(X[:, 0], X[:, 1], c=y)
-    ax.ticklabel_format(axis='both', style='sci', scilimits=(-1, 1), useMathText=True)
-    ax.set_xlabel(r'$\tilde{x}$')
-    ax.set_ylabel(r'$\tilde{y}$')
-    ax.set_box_aspect(1)
+    # Remove the ticks
+    ax.set_xticks([])
+    ax.set_yticks([])
+    # Remove the tick labels
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    # ax.set_xlabel(r'$\Psi_1$')
+    # ax.set_ylabel(r'$\Psi_2$')
+    ax = set_equal_ranges(ax) # ax.set_box_aspect(1)
 
     for format in ('.pdf', '.png', '.svg'):
-        fig.savefig(os.path.join(output_dir, filename + format))
+        fig.savefig(path.join(output_dir, filename + format))
     
     plt.close(fig)
 
 
-def plot_history(history, output_dir, log_scale=False):
+def plot_history(history, output_dir, filename, log_scale=False):
     os.makedirs(output_dir, exist_ok=True)
 
-    h = history.history
-    keys = [key for key in h.keys() if not key.startswith('val_')]
+    keys = [key for key in history.keys() if not key.startswith('val_')]
     for key in keys:
-        y = np.array([h[key], h['val_' + key]])
+        y = np.array([history[key], history['val_' + key]])
         fig, ax = plt.subplots()
         if log_scale:
             ax.semilogy(y[0], label='Training')
@@ -85,6 +94,49 @@ def plot_history(history, output_dir, log_scale=False):
         ax.set_xlabel('Epoch')
         ax.legend()
         for format in ('.pdf', '.png', '.svg'):
-            fig.savefig(os.path.join(output_dir, key + format))
+            fig.savefig(path.join(output_dir, filename + '-' + key + format))
         
         plt.close(fig)
+
+
+root = 'experiments/helix/results'
+titles = [
+    'Few samples without noise',
+    'Many samples without noise',
+    'Few samples with noise',
+    'Many samples with noise'
+]
+results_file = 'results.h5'
+history_file = 'history.h5'
+
+q_vals = [2e-2, 2e-2, 5e-3, 5e-3]
+steps_vals = [100, 100, 100, 100]
+alpha_vals = [1, 1, 1, 1]
+
+for i in range(len(titles)):
+    title = titles[i]
+    q, steps, alpha = q_vals[i], steps_vals[i], alpha_vals[i]
+    experiment = f'quantile_{q}-steps_{steps}-alpha_{alpha}'
+    output_dir = path.join(root, title, experiment)
+    history = {}
+    with h5py.File(path.join(output_dir, history_file), 'r') as file:
+        for key in file.keys():
+            history[key] = np.array(file[key])
+
+    plot_history(history, output_dir, 'history', log_scale=True)
+
+    for subset in ('train', 'test'):
+        with h5py.File(path.join(output_dir, results_file), 'r') as file:
+            X_orig = np.array(file['/X_' + subset])
+            X_red = np.array(file['/X_' + subset + '_red'])
+            X_rec = np.array(file['/X_' + subset + '_rec'])
+            y = np.array(file['/y_' + subset])
+            if subset == 'train':
+                P_color = np.array(file['P_color'])
+                D_color = np.array(file['D_color'])
+                plot_original(X_orig, P_color, output_dir, 'train_P')
+                plot_original(X_orig, D_color, output_dir, 'train_D')
+                    
+        plot_original(X_orig, y, output_dir, subset + '_orig')
+        plot_projection(X_red, y, output_dir, subset + '_red')
+        plot_original(X_rec, y, output_dir, subset + '_rec')

experiments/mnist/experiment.py
@@ -1,18 +1,26 @@
 import time
 import numpy as np
 import tensorflow as tf
+import os
+import random
 from os import path
+import h5py
 
 from DiffusionMaps import DiffusionMaps
 from aux_functions import get_sigma
-from experiments.mnist.plot_results import plot_original, plot_projection, plot_interpolations, plot_history
 from experiments.mnist.load_data import get_datasets
-from experiments.mnist.metrics import compute_metrics
 from experiments.utils import build_conv_decoder
 
 
+# ENSURE REPRODUCIBILITY
 seed = 123
+os.environ['PYTHONHASHSEED'] = str(seed)
+random.seed(seed)
+np.random.seed(seed)
 tf.random.set_seed(seed)
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1' # force the use of CPU
+
+
 root = 'experiments/mnist/results'
 titles = [
     'Few samples without noise',
@@ -29,14 +37,15 @@ alpha_vals = [0, 0, 0, 0]
 kernel = 'rbf'
 
 for i in range(len(titles)):
+    title = titles[i]
     q, steps, alpha = q_vals[i], steps_vals[i], alpha_vals[i]
-    experiment = f'percentile_{q}-steps_{steps}-alpha_{alpha}'
+    experiment = f'quantile_{q}-steps_{steps}-alpha_{alpha}'
+    output_dir = path.join(root, title, experiment)
     X_train, y_train = datasets_train[i]
     X_test, y_test = datasets_test[i]
     # Añadimos la dimensión de canal
     X_train = np.expand_dims(X_train, axis=-1)
     X_test = np.expand_dims(X_test, axis=-1)
-    title = titles[i]
     sigma = get_sigma(X_train.reshape((X_train.shape[0], -1)), q)
     
     print(experiment, '-', title)  
@@ -53,62 +62,26 @@ for i in range(len(titles)):
     X_train_rec = decoder(X_train_red).numpy()
     X_test_rec = decoder(X_test_red).numpy()
     
+    decoder.save(path.join(output_dir, 'decoder.h5'))
+    with h5py.File(path.join(output_dir, 'history.h5'), 'w') as file:
+        for key, value in history.history.items():
+            file.create_dataset(key, data=value)
+    
+    X_train = X_train.reshape(X_train.shape[0], -1)
+    X_train_rec = X_train_rec.reshape(X_train_rec.shape[0], -1)
+    X_test = X_test.reshape(X_test.shape[0], -1)
+    X_test_rec = X_test_rec.reshape(X_test_rec.shape[0], -1)
+    with h5py.File(path.join(output_dir, 'results.h5'), "w") as file:
+        file.create_dataset("X_train", data=X_train, compression='gzip')
+        file.create_dataset("X_train_red", data=X_train_red, compression='gzip')
+        file.create_dataset("X_train_rec", data=X_train_rec, compression='gzip')
+        file.create_dataset("y_train", data=y_train, compression='gzip')
+        file.create_dataset("X_test", data=X_test, compression='gzip')
+        file.create_dataset("X_test_red", data=X_test_red, compression='gzip')
+        file.create_dataset("X_test_rec", data=X_test_rec, compression='gzip')
+        file.create_dataset("y_test", data=y_test, compression='gzip')
+
     time_in_sample = tac - tic
     time_out_of_sample = toc - tac
-    
-    plot_original(
-        X_train, y_train,
-        path.join(root, title), 'train_orig',
-        images_per_class=2, grid_shape=(3, 4)
-    )
-    plot_original(
-        X_test, y_test,
-        path.join(root, title), 'test_orig',
-        images_per_class=2, grid_shape=(3, 4)
-    )
-    plot_projection(X_train_red, y_train, path.join(root, title, experiment), 'train_red',)
-    plot_original(
-        X_train_rec, y_train,
-        path.join(root, title, experiment), 'train_rec',
-        images_per_class=2, grid_shape=(3, 4)
-    )
-    plot_projection(X_test_red, y_test, path.join(root, title, experiment), 'test_red',)
-    plot_original(
-        X_test_rec, y_test,
-        path.join(root, title, experiment), 'test_rec',
-        images_per_class=2, grid_shape=(3, 4)
-    )
-    plot_interpolations(
-        X_train_red,
-        y_train,
-        decoder,
-        path.join(root, title, experiment),
-        'train_interp',
-        class_pairs = [(i, i+1) for i in range(0, 5)],
-        n_interpolations=6
-    )
-    plot_interpolations(
-        X_test_red,
-        y_test,
-        decoder,
-        path.join(root, title, experiment),
-        'test_interp',
-        class_pairs = [(i, i+1) for i in range(0, 5)],
-        n_interpolations=6
-    )
-    plot_history(history, path.join(root, 'histories', title, experiment), log_scale=True)
-
-    compute_metrics(
-        X_train,
-        y_train,
-        X_train_red,
-        X_train_rec,
-        X_test,
-        y_test,
-        X_test_red,
-        X_test_rec,
-        time_in_sample,
-        time_out_of_sample,
-        title,
-        output_dir=path.join(root, title, experiment)
-    )
+    times = np.array([time_in_sample, time_out_of_sample])
+    np.savetxt(path.join(output_dir, 'times.txt'), times)

experiments/phoneme/plot_results.py
@@ -1,10 +1,38 @@
 import os
+from os import path
+import h5py
 import numpy as np
 from matplotlib import pyplot as plt
 
 plt.style.use('experiments/science.mplstyle')
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 classes = {0: 'aa', 1: 'ao', 2: 'dcl', 3: 'iy', 4: 'sh'}
+
+
+def set_equal_ranges(ax):
+    # Get the current axis limits
+    x_min, x_max = ax.get_xlim()
+    y_min, y_max = ax.get_ylim()
+    
+    # Calculate the ranges of the x and y axes
+    x_range = x_max - x_min
+    y_range = y_max - y_min
+
+    # Find the maximum range between x and y
+    max_range = max(x_range, y_range)
+
+    # Set new limits with the same range for both axes
+    x_center = (x_max + x_min) / 2
+    y_center = (y_max + y_min) / 2
+
+    ax.set_xlim(x_center - max_range / 2, x_center + max_range / 2)
+    ax.set_ylim(y_center - max_range / 2, y_center + max_range / 2)
+
+    # Set equal aspect ratio
+    ax.set_aspect('equal', adjustable='box')
+
+    return ax
+
 
 def plot_original(X, y, output_dir, filename, n_samples=30):
     os.makedirs(output_dir, exist_ok=True)
@@ -24,7 +52,7 @@ def plot_original(X, y, output_dir, filename, n_samples=30):
 
 
     for format in ('.pdf', '.png', '.svg'):
-        fig.savefig(os.path.join(output_dir, filename + format))
+        fig.savefig(path.join(output_dir, filename + format))
     
     plt.close(fig)
 
@@ -34,11 +62,15 @@ def plot_projection(X, y, output_dir, filename):
 
     fig, ax = plt.subplots(figsize=(3, 3), constrained_layout=True)
     ax.scatter(X[:, 0], X[:, 1], c=[colors[i] for i in y])#y, cmap=cmap)
-    ax.ticklabel_format(axis='both', style='sci', scilimits=(-2, 2), useMathText=True)
-    # Set box aspect ratio instead of axis aspect to ensure square subplots
-    ax.set_xlabel(r'$\tilde{x}$')
-    ax.set_ylabel(r'$\tilde{y}$')
-    ax.set_box_aspect(1)
+    # Remove the ticks
+    ax.set_xticks([])
+    ax.set_yticks([])
+    # Remove the tick labels
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    # ax.set_xlabel(r'$\Psi_1$')
+    # ax.set_ylabel(r'$\Psi_2$')
+    ax = set_equal_ranges(ax) # ax.set_box_aspect(1)
 
     # Create a list of handles and labels for the legend
     unique_y = np.unique(y)
@@ -49,18 +81,17 @@ def plot_projection(X, y, output_dir, filename):
     fig.legend(handles, labels, loc='lower center', ncol=len(unique_y), columnspacing=0.5, bbox_to_anchor=(0.5, -0.1))
 
     for format in ('.pdf', '.png', '.svg'):
-        fig.savefig(os.path.join(output_dir, filename + format))
+        fig.savefig(path.join(output_dir, filename + format))
     
     plt.close(fig)
 
 
-def plot_history(history, output_dir, log_scale=False):
+def plot_history(history, output_dir, filename, log_scale=False):
     os.makedirs(output_dir, exist_ok=True)
 
-    h = history.history
-    keys = [key for key in h.keys() if not key.startswith('val_')]
+    keys = [key for key in history.keys() if not key.startswith('val_')]
     for key in keys:
-        y = np.array([h[key], h['val_' + key]])
+        y = np.array([history[key], history['val_' + key]])
         fig, ax = plt.subplots()
         if log_scale:
             ax.semilogy(y[0], label='Training')
@@ -68,11 +99,50 @@ def plot_history(history, output_dir, log_scale=False):
         else:
             ax.plot(y[0], label='Training')
             ax.plot(y[1], label='Validation')
-
+            ax.ticklabel_format(axis='both', style='sci', scilimits=(-1, 1), useMathText=True)
+        
         ax.set_ylabel(key.capitalize())
         ax.set_xlabel('Epoch')
         ax.legend()
         for format in ('.pdf', '.png', '.svg'):
-            fig.savefig(os.path.join(output_dir, key + format))
+            fig.savefig(path.join(output_dir, filename + '-' + key + format))
         
         plt.close(fig)
+
+
+root = 'experiments/phoneme/results'
+titles = [
+    'Few samples without noise',
+    'Many samples without noise',
+    'Few samples with noise',
+    'Many samples with noise'
+]
+results_file = 'results.h5'
+history_file = 'history.h5'
+
+q_vals = [0.75, 0.75, 0.75, 0.75]
+steps_vals = [1, 1, 1, 1]
+alpha_vals = [0, 0, 0, 0]
+
+for i in range(len(titles)):
+    title = titles[i]
+    q, steps, alpha = q_vals[i], steps_vals[i], alpha_vals[i]
+    experiment = f'quantile_{q}-steps_{steps}-alpha_{alpha}'
+    output_dir = path.join(root, title, experiment)
+    history = {}
+    with h5py.File(path.join(output_dir, history_file), 'r') as file:
+        for key in file.keys():
+            history[key] = np.array(file[key])
+
+    plot_history(history, output_dir, 'history', log_scale=True)
+
+    for subset in ('train', 'test'):
+        with h5py.File(path.join(output_dir, results_file), 'r') as file:
+            X_orig = np.array(file['/X_' + subset])
+            X_red = np.array(file['/X_' + subset + '_red'])
+            X_rec = np.array(file['/X_' + subset + '_rec'])
+            y = np.array(file['/y_' + subset])
+        
+        plot_original(X_orig, y, output_dir, subset + '_orig')
+        plot_projection(X_red, y, output_dir, subset + '_red')
+        plot_original(X_rec, y, output_dir, subset + '_rec')

experiments/swiss_roll/load_data.py
@@ -2,11 +2,32 @@ import numpy as np
 from sklearn.datasets import make_swiss_roll
 from sklearn.model_selection import train_test_split
 
-from experiments.swiss_roll.plot_results import my_colormap2D
-
 
 def normalize(x):
     return (x - np.min(x)) / (np.max(x) - np.min(x))
+
+
+def my_colormap1D(x):
+    c1=(0.75, 0, 0.75)
+    c2=(0, 0.75, 0.75)
+    # Calculate the RGB values based on interpolation
+    color = np.array(c1) * (1 - x) + np.array(c2) * x
+
+    return color
+
+
+def my_colormap2D(x, y):
+    # Define colors in RGB
+    bottom_left = (0.5, 0, 0.5) # dark magenta
+    bottom_right = (0, 0.5, 0.5) # dark cyan
+    top_left = (1, 0, 1) # magenta
+    top_right = (0, 1, 1) # cyan
+
+    # Calculate the RGB values based on interpolation
+    top_color = np.array(top_left) * (1 - x) + np.array(top_right) * x
+    bottom_color = np.array(bottom_left) * (1 - x) + np.array(bottom_right) * x
+
+    return top_color * (1 - y) + bottom_color * y
 
 
 def get_data(npoints=2000, test_size=0.5, seed=123, noise=0):